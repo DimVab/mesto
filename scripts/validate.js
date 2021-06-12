a69fd17190412a05e4a5cf744777cf557346c966
Dmitry Vabischevich <Dmitryvabischevich@gmail.com>
2021-06-12 16:56:57 +0300

    feat: добавить функцию проверки валидации для одного поля

--- scripts/validate.js
+++ scripts/validate.js
@@ -2,7 +2,7 @@
 1. Объявить нужные переменные
 2. Объявить функцию показа ошибки +
 3. Объявить функцию скрытия ошибки +
-4. Объявить функцию проверки валидации для одного поля
+4. Объявить функцию проверки валидации для одного поля +
 5. Объявить функцию проверки валидации для всех полей (для кнопки)
 6. Объявить функцию изменения состояния кнопки (если выполняется п.5)
 7. Объявить функцию навешивания функций п.4 и функции 6 в начале функции и при навешивании функций п.4 на все поля
@@ -26,14 +26,25 @@ function showInputError(formElement, inputElement, errorMessage) {
   // не добавляется класс стилизации input при ошибке валидации, тк input стилизуется при помощи псевдокласса :invalid
 }
 
-function hideInputError(formElement, inputElement, errorMessage) {
+function hideInputError(formElement, inputElement) {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   errorElement.textContent = '';
   errorElement.classList.remove('popup__input-error_active');
   console.log('Функция hideInputError выполнена'); /* потом удалить */
 }
 
+function isValid(formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+  console.log('Функция isValid выполнена'); /* потом удалить */
+}
+
 // тестовые вызовы функций
-showInputError(form1, input1, error1);
-hideInputError(form1, input1);
+// showInputError(form1, input1, error1);
+// hideInputError(form1, input1);
+isValid(form1, input1);
+
 // рабочие вызовы функций:
